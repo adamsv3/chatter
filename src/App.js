df7993a8cc83108bc05c24101634b794c60dd7f6
Valerie Adams <adamsv3@uw.edu>
2022-01-21 16:27:18 -0800

    added comments

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="header">
+      <header className="header"> /* keeps div at top of page */
         <div className="logo" />
         <span className="title">CHATTER!</span>
       </header>
-      <footer className="footer">
-        <input className="text-input" />
+      <footer className="footer"> /* keeps div at bottom of page */
+        <input className="text-input" /> /* allows users to input their own text */
         <button className="send">↑</button>
       </footer>
     </div>
